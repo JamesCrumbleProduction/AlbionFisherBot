c3384fe05778baa1850339897219e55c1e299c95
Artem Traschinsky <namster222333@gmail.com>
2022-09-05 22:08:52 +0200

    refactor: refactor bot architecture

---
+++ bot/general/settings.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+from dataclasses import dataclass
+from pydantic import BaseSettings
+
+
+class HSVConfigs(BaseSettings):
+
+    @dataclass
+    class Bobber:
+        # [Hmin, Smin, Vmin]
+        LOWER_HSV_ARRAY = np.array(
+            [0, 157, 206],
+            dtype=np.uint8, copy=False
+        )
+        # [Hmax, Smax, Vmax]
+        HIGHER_HSV_ARRAY = np.array(
+            [3, 224, 255],
+            dtype=np.uint8, copy=False
+        )
+
+
+class BotSettings(BaseSettings):
+
+    # fishing rod throwing delay
+    THROW_DELAY: float = 0.6
+    BOBBER_CATCH_THRESHOLD: int = 45  # in percentage
+    MOUSE_CATCHING_BAR_THRESHOLD: int = 60  # in percentage
+
+
+class IOServiceSettings(BaseSettings):
+
+    CLICK_INTERVAL: float = 0.1
+
+
+class Settings(BaseSettings):
+
+    BOT = BotSettings()
+    HSV_CONFIGS = HSVConfigs()
+    IO_SERVICE = IOServiceSettings()
+
+
+settings = Settings()
