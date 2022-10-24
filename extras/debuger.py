fe582d7bb6ed08d0e2fd6382a843ddd580ccfa09
Тращинский Артем <atraschinskiy@innoseti.ru>
2022-10-24 17:11:51 +0200

    debug: add template validation debugging

--- extras/debuger.py
+++ extras/debuger.py
@@ -4,7 +4,18 @@ import time
 import numpy as np
 
 from mss import mss
+from pydantic import BaseModel
 from screeninfo import get_monitors
+
+
+class ValidatedTemplateData(BaseModel):
+    location_x: np.ndarray
+    location_y: np.ndarray
+    height: int
+    width: int
+
+    class Config:
+        arbitrary_types_allowed = 'allow'
 
 
 def _define_monitor_region() -> dict:
@@ -64,3 +75,94 @@ def regions():
             cv2.imwrite('test.png', image)
 
         time.sleep(0.2)
+
+
+def serching_template():
+
+    REGION: dict[str, int] = dict(
+        top=60,
+        left=50,
+        height=89,
+        width=214
+    )
+
+    def region() -> dict:
+        for value in REGION.values():
+            if value != 0:
+                break
+        else:
+            return MONITOR
+
+        return REGION
+
+    current_path = os.getcwd()
+    templates_dir = os.path.join(
+        current_path, 'bot', 'general', 'components', 'templates', 'raw_templates', 'fisher_bot', 'buffs', 'bait', 'is_active'
+    )
+
+    templates: list[np.ndarray] = [
+        cv2.cvtColor(
+            cv2.imread(
+                template_path.path
+            ), cv2.COLOR_BGR2RGB
+
+        ) for template_path in os.scandir(templates_dir)
+    ]
+
+    def validate_template(img, template, threshold):
+        height, width = template.shape[:-1]
+        res = cv2.matchTemplate(
+            img,
+            template,
+            cv2.TM_CCOEFF_NORMED
+        )
+        location_y, location_x = np.where(res >= threshold)
+
+        if location_y.size > 0 and location_x.size > 0:
+            return ValidatedTemplateData(
+                location_x=location_x,
+                location_y=location_y,
+                height=height,
+                width=width
+            )
+
+    while True:
+
+        with mss() as base:
+            image = cv2.cvtColor(
+                np.array(base.grab(region()), dtype=np.uint8),
+                cv2.COLOR_BGR2RGB
+            )
+
+            for i, template in enumerate(templates):
+
+                template_data = validate_template(
+                    image, template, threshold=0.7
+                )
+                print(
+                    f'TEMPLATE NUMBER "{i}" IS FOUNDED => "{bool(template_data)}"'
+                )
+                if template_data is None:
+                    continue
+
+                if template_data := validate_template(
+                    image, template, threshold=0.7
+                ):
+
+                    for x, y in zip(template_data.location_x, template_data.location_y):
+                        image = cv2.rectangle(
+                            image, (x, y),
+                            (
+                                x + template_data.width,
+                                y + template_data.height
+                            ),
+                            (0, 0, 255), 1
+                        )
+                    break
+
+            cv2.imwrite('test.png', image)
+
+        time.sleep(0.2)
+
+
+serching_template()
