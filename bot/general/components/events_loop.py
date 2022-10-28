3a3cd4dcdb46576c2eec110ab2296223bac76296
Тращинский Артем <atraschinskiy@innoseti.ru>
2022-10-28 17:23:16 +0200

    fix: fix region define

--- bot/general/components/events_loop.py
+++ bot/general/components/events_loop.py
@@ -1,6 +1,5 @@
 import time
 
-from functools import partial
 from typing import TYPE_CHECKING
 from pynput.mouse import Listener, Button
 
@@ -15,26 +14,35 @@ class EventsLoop:
 
     __slots__ = (
         '_bot_instance',
-        '_new_first_custom_catching_coord',
+        '_new_first_catching_coord',
     )
 
     def __init__(self, bot_instance: 'FisherBot'):
         self._bot_instance = bot_instance
-        self._new_first_custom_catching_coord: tuple[int, int] = None
+        self._new_first_catching_coord: tuple[int, int] = None
 
     def _mouse_on_click_event(self, x: int, y: int, button: Button, is_pressed: bool):
         if button is Button.middle:
             if is_pressed:
-                self._new_first_custom_catching_coord = x, y
+                self._new_first_catching_coord = x, y
             else:
+                left_top_coord = min(
+                    [self._new_first_catching_coord, (x, y)],
+                    key=lambda v: v[0]
+                )
+                right_bottom_coord = max(
+                    [self._new_first_catching_coord, (x, y)],
+                    key=lambda v: v[0]
+                )
                 self._bot_instance.set_new_catching_region(
                     Region(
-                        top=self._new_first_custom_catching_coord[1],
-                        left=self._new_first_custom_catching_coord[0],
-                        width=x, height=y
+                        top=left_top_coord[1],
+                        left=left_top_coord[0],
+                        width=right_bottom_coord[0],
+                        height=right_bottom_coord[1]
                     )
                 )
-                self._new_first_custom_catching_coord = None
+                self._new_first_catching_coord = None
 
     def __call__(self):
         mouse_listener: Listener = None
@@ -44,9 +52,7 @@ class EventsLoop:
                 self._bot_instance.change_status(Status.PAUSED)
 
             if self._bot_instance.status is Status.PAUSED and mouse_listener is None:
-                mouse_listener = Listener(
-                    on_click=self._mouse_on_click_event
-                )
+                mouse_listener = Listener(on_click=self._mouse_on_click_event)
                 mouse_listener.start()
 
             elif self._bot_instance.status is Status.CATCHING:
