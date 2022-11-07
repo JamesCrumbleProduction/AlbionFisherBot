9142e30d24e36f3a4089431226d88cfe6768d56c
Тращинский Артем <atraschinskiy@innoseti.ru>
2022-11-08 00:38:44 +0200

    feat: update auto eat script

--- bot/extras/auto_eat.py
+++ bot/extras/auto_eat.py
@@ -8,9 +8,16 @@ from pynput.keyboard import Controller as KeyboardController
 mouse_controller = MouseController()
 keyboard_controller = KeyboardController()
 
-time.sleep(5)
+time.sleep(2.5)
+
+mouse_position = mouse_controller.position
+print('MOUSE POSITION WAS SAVED')
+
+time.sleep(2.5)
 
 while True:
+    if mouse_controller.position[0] != mouse_position[0] or mouse_controller.position[1] != mouse_position[1]:
+        mouse_controller.position = mouse_position
     keyboard_controller.press('2')
     keyboard_controller.release('2')
     time.sleep(1.1)
